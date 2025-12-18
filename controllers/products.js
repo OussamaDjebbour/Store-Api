b0da56d32e822ba5f33b66cca8912716242ceb22
OussamaDjebbour <oussama_djebbour@outlook.com>
2025-12-18 21:46:57 +0100

    Implement skip and limit filter functionality

--- controllers/products.js
+++ controllers/products.js
@@ -37,6 +37,12 @@ export const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
