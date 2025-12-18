92d141a231a1fd0b2e7ea6fe49a1a94f36f17d13
OussamaDjebbour <oussama_djebbour@outlook.com>
2025-12-18 16:53:29 +0100

    Implement the select filter functionality

--- controllers/products.js
+++ controllers/products.js
@@ -4,12 +4,12 @@ export const getAllProductsStatic = async (req, res) => {
   // const search = 'din';
   const products = await Product.find({
     // name: { $regex: search, $options: 'i' },
-  }).sort('name');
+  }).select('name createdAt price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 export const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -30,6 +30,11 @@ export const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.trim().split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result;
